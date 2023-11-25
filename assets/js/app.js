3970d3ad945423e9f4e9cc24e54207e871187a38
Shiv-468 <shivmuwal.147@gmail.com>
2023-11-25 14:02:20 +0530

    _change in navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-const menuIcon = document.querySelector(".menuIcon");
-const menuList = document.querySelector(".menuList");
+let menuIcon = document.querySelector(".menuIcon");
+let menuList = document.querySelector(".menuList");
 menuIcon.addEventListener("click", function () {
     menuList.classList.toggle("show");
 });
